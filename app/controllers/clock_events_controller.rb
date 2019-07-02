73c8535f3b4df0405d5a0dbeb5da38fc63123bad
Hamsharan Mahalingam <hamsharan@gmail.com>
2019-07-01 23:42:24 -0400

    displaying results

--- app/controllers/clock_events_controller.rb
+++ app/controllers/clock_events_controller.rb
@@ -10,11 +10,11 @@ class ClockEventsController < ApplicationController
     last_name = params[:clock_events][:last_name]
     clocktype = params[:clock_events][:clock_event]
 
-    user = User.find_or_create_by(first_name:first_name,last_name:last_name)
-    ClockEvent.create({user:user,clocktype:clocktype, time:DateTime.now})
+    @user = User.find_or_create_by(first_name:first_name,last_name:last_name)
+    ClockEvent.create({user:@user,clocktype:clocktype, time:DateTime.now})
 
-    user.clock_events
-    render redirect:index
+    @clocked_events = @user.clock_events
+
   end
 
   def edit
