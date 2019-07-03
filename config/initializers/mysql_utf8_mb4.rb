369029104a22fef0136b593bd7bd9ad2f7661799
Hamsharan Mahalingam <hamsharan@gmail.com>
2019-07-03 19:56:38 -0400

    monkey patch variables

--- config/initializers/mysql_utf8_mb4.rb
+++ config/initializers/mysql_utf8_mb4.rb
@@ -16,5 +16,17 @@ if ENV['USE_UTF8MB4'] && config['adapter'] == 'mysql2'
       sql_options = "ROW_FORMAT=DYNAMIC #{sql_options}" unless sql_options.to_s.include?("ROW_FORMAT")
       super(table_name, options.merge(options: sql_options))
     end
+
+  def create_database(name, options = {collation:'utf8mb4_bin',charset:'utf8mb4',innodb_large_prefix:1})
+      if options[:collation]
+        execute "CREATE DATABASE #{quote_table_name(name)} DEFAULT COLLATE #{quote_table_name(options[:collation])}"
+      elsif options[:charset]
+        execute "CREATE DATABASE #{quote_table_name(name)} DEFAULT CHARACTER SET #{quote_table_name(options[:charset])}"
+      elsif row_format_dynamic_by_default?
+        execute "CREATE DATABASE #{quote_table_name(name)} DEFAULT CHARACTER SET `utf8mb4`"
+      else
+        raise "Configure a supported :charset and ensure innodb_large_prefix is enabled to support indexes on varchar(255) string columns."
+      end
+    end
   end
 end
